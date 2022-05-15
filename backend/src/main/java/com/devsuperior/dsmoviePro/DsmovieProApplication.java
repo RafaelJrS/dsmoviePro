4c1328047c11b9d5ebb5bfea0e5df1ec7c9b9b47
RafaelJrS <silvarafaeljr@gmail.com>
2022-05-15 01:33:22 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmoviePro/DsmovieProApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmoviePro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieProApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmoviePro/DsmovieProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmoviePro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
